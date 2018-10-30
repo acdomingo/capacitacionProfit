749a209e7c358e1e60b8b639a79e4a9cf2873227
acdomingo <acdomingo@profitsistemas.com>
2018-10-30 11:42:50 -0300

    Video 06 - 30/10/18

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Divider, Segment, Container, Button, Form } from 'semantic-ui-react';
-import { string, number } from 'prop-types';
 
 export class App extends React.Component<{}, IState> {
 
@@ -18,16 +17,29 @@ export class App extends React.Component<{}, IState> {
             currentTask: '',
             tasks: [
                 ...this.state.tasks,
-                this.state.currentTask
+                {
+                    id: this._timeInMilliseconds(),
+                    value: this.state.currentTask,
+                    completed: false
+                }
             ]
+        })
+    }
+
+    public deleteTask(id:number):void {
+        const filteredTasks: Array<ITask> = this.state.tasks.filter((task: ITask) => task.id !== id);
+        this.setState ({
+            tasks: filteredTasks
         })
     }
 
     public renderTasks():JSX.Element[] {
         return (
-            this.state.tasks.map((task:string, index:number) => {
+            this.state.tasks.map((task:ITask, index:number) => {
                 return (
-                    <div key={index}> {task}
+                    <div key={task.id}>
+                    <span> {task.value} </span>
+                    <Button compact circular toggle size='tiny' color='violet' onClick={() => this.deleteTask(task.id) }>Delete</Button>
                         <Divider fitted />
                     </div>
                 )
@@ -67,9 +79,20 @@ export class App extends React.Component<{}, IState> {
             </div>
         )
     }
+
+    private _timeInMilliseconds():number {
+        const date:Date = new Date();
+        return date.getTime()
+    }
 }
 
 interface IState {
     currentTask: string,
-    tasks : Array<string>
+    tasks : Array<ITask>
+}
+
+interface ITask {
+    id: number,
+    value: string,
+    completed: boolean
 }
